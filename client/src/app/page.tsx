857293623d7a89a2dd6b0f2c614ef6654baf85f8
AminMasoudi <aminmasoudi2003@gmail.com>
2025-11-27 11:44:10 +0330

    starting a new client next.js project

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div className="mx-36 my-20">
+<h1 className="text-3xl">Whisper</h1>
+<p>an E2EE messanger project</p>
+</div>
+  );
+}
