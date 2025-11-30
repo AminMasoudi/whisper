252809a1002b54cf871778e5d7f33d147c69ce73
AminMasoudi <aminmasoudi2003@gmail.com>
2025-11-30 19:25:15 +0330

    [BACKEND] : UserCrud.retrieve

--- server/app/db/crud/user_crud.py
+++ server/app/db/crud/user_crud.py
@@ -8,9 +8,13 @@ class UserCrud(BaseCrud):
     @staticmethod
     def create(session: Session, username: str, hashed_password: str) -> User:
         e = session.get(User, username)
-        if e: raise HTTPException(400)
+        if e: raise HTTPException(409, "Username is duplicated.")
         user = User(username=username, password=hashed_password)
         session.add(user)
         session.commit()
         session.refresh(user)
         return user
+    
+    @staticmethod
+    def retrieve(session: Session, username: str) -> User | None:
+        return session.get(User, username)
