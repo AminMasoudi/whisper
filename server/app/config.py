a76b450a4da06d63251fb8a7ffc440cf37a31618
AminMasoudi <aminmasoudi2003@gmail.com>
2025-11-30 19:24:35 +0330

    [BACKEND] : create a config class

--- server/app/config.py
+++ server/app/config.py
@@ -1,2 +1,15 @@
+from datetime import timedelta, timezone
+from os import environ
+from dotenv import load_dotenv
 
-class Config: ...
+load_dotenv()
+
+
+class Config:
+    SECRETE_KEY = environ.get("SECRETE_KEY")
+    TIME_ZONE = timezone(timedelta(minutes=int(environ.get("TZ_OFFSET", "210"))))
+    JWT_EXP_TIMEDELTA = timedelta(minutes=int(environ.get("TOKEN_EXP", "20")))
+    JWT_ALGORITHM = "HS256"
+
+
+config = Config()
