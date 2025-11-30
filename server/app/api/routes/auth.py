4a5f37b5687c9e27ab1493a9036232c66332e5db
AminMasoudi <aminmasoudi2003@gmail.com>
2025-11-30 19:23:59 +0330

    [BACKEND] : add login

--- server/app/api/routes/auth.py
+++ server/app/api/routes/auth.py
@@ -2,11 +2,16 @@ from fastapi import APIRouter
 from schemas.user_schema import AuthRegister
 from db.database import SessionDep
 from services.auth_service import AuthService
+from utils.security import TokenFormLogin
 
 router = APIRouter()
 
 
-@router.post("/auth/register")
+@router.post("/register")
 def register(user: AuthRegister, session: SessionDep):
     user = AuthService.register(session, user.username, user.password)
     return {"username": user.username}
+
+@router.post("/login")
+def login(form_data:  TokenFormLogin, session:SessionDep):
+    return {"access_token": AuthService.login(session, form_data.username, form_data.password), "token_type": "bearer"}
