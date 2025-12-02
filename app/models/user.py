c0fec66fe90e55337ea387f298c28dd78005205e
amin masoudi <aminmasoudi2003@gmail.com>
2025-12-02 22:38:07 +0330

    update usermodel

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 from sqlmodel import Field, SQLModel
+from uuid import UUID, uuid4
 
+from utils.helper import now
 
 class User(SQLModel, table=True):
-    username: str = Field(index=True, primary_key=True)
+    #TODO: add display_name
+    id: UUID = Field(primary_key=True, default_factory=uuid4)
+    username: str = Field(index=True, unique=True)
+    display_name: str = Field()
     password: str = Field()
     is_active: bool = Field(default=True)
-    created_at: datetime = Field(default=datetime.now())
+    created_at: datetime = Field(default_factory=now)
